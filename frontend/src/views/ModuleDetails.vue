9b84c190b092fb1120886b81cebd92fa5e3bea9d
junzhi <s10195174@connect.np.edu.sg>
2022-02-07 05:13:18 +0800

    Fix json parsing

--- frontend/src/views/ModuleDetails.vue
+++ frontend/src/views/ModuleDetails.vue
@@ -86,7 +86,7 @@ export default {
             .then(resp => resp.json())
             .then(data => {
               this.students = data.enrolled_students
-              this.tutors = data.assigned_tutors
+              this.tutors = data.assigned_tutors[0]
               this.classes = data.classes
               this.link = data.ratings_and_comments_url
             })
